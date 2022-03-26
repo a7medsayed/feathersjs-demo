c3766c5d83e8140abe7d04ffdda63e3a04411f27
ahmed sayed <ahmedsayedmohammed93@gmail.com>
2022-03-26 16:17:44 +0200

    add port variable

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import logger from './logger';
 import app from './app';
 
-const port = app.get('port');
-const server = app.listen(port);
+const port = app.get('port') || process.env.PORT;
+const server = app.listen(port) ;
 
 process.on('unhandledRejection', (reason, p) =>
   logger.error('Unhandled Rejection at: Promise ', p, reason)
